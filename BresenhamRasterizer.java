a3f4438f8c54b5fbeb98581ce45c4646a01226f0
bigLavaStone <mrityunjoysahoo007@gmail.com>
2026-02-08 20:55:55 +0530

    init bresenham class struct

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,6 +2,14 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
+        // Initialization
+        ArrayList<Point> points = new ArrayList<>();
+
+        int x1 = p1.x;
+        int y1 = p1.y;
+        int x2 = p2.x;
+        int y2 = p2.y;
+
         return new Point[]{ new Point(0,0), new Point(2,3) };
     }
 
