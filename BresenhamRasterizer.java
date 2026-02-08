42a8a704313b245e8b28d83e1aee227ec94a5df5
bigLavaStone <mrityunjoysahoo007@gmail.com>
2026-02-08 21:14:31 +0530

    slope & delta calculation

--- BresenhamRasterizer.java
+++ BresenhamRasterizer.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 class BresenhamRasterizer implements LineRasterizer {
     public Point[] rasterize(Point p1, Point p2) {
+
         // Initialization
         ArrayList<Point> points = new ArrayList<>();
 
@@ -9,6 +10,16 @@ class BresenhamRasterizer implements LineRasterizer {
         int y1 = p1.y;
         int x2 = p2.x;
         int y2 = p2.y;
+
+        // Endpoint extraction logic
+        // Delta & Slope Calculation
+        int dx = Math.abs(x2 - x1);
+        int dy = Math.abs(y2 - y1);
+
+        int sx = (x1 < x2) ? 1 : -1;
+        int sy = (y1 < y2) ? 1 : -1;
+
+        int err = dx - dy;
 
         return new Point[]{ new Point(0,0), new Point(2,3) };
     }
