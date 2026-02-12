03f753c4961fb09d737e1018a6d86004857885d7
bigLavaStone <mrityunjoysahoo007@gmail.com>
2026-02-12 23:00:39 +0530

    wu slope difference computation

--- WuRasterizer.java
+++ WuRasterizer.java
@@ -12,6 +12,25 @@ public class WuRasterizer implements LineRasterizer{
 
         boolean steep = Math.abs(y2 - y1) > Math.abs(x2 - x1);
 
+        // Delta & Slope Calculation
+        if (steep) {
+            int temp;
+            temp = x1; x1 = y1; y1 = temp;
+            temp = x2; x2 = y2; y2 = temp;
+        }
+
+        if (x1 > x2) {
+            int temp;
+            temp = x1; x1 = x2; x2 = temp;
+            temp = y1; y1 = y2; y2 = temp;
+        }
+
+        float dx = x2 - x1;
+        float dy = y2 - y1;
+        float gradient = dy / dx;
+
+        float y = y1;
+
         return points.toArray(new Point[0]);
     }    
 }
