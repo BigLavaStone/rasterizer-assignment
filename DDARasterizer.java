d5b93a1450a6b56fdab138aa463d1426162bb90e
bigLavaStone <mrityunjoysahoo007@gmail.com>
2026-02-12 22:55:25 +0530

    dda slope difference computation

--- DDARasterizer.java
+++ DDARasterizer.java
@@ -2,11 +2,21 @@ import java.util.ArrayList;
 
 public class DDARasterizer implements LineRasterizer  {
     public Point[] rasterize(Point p1, Point p2) {
-        // Initialized DDA rasterizer structure
+        // Initialization
         ArrayList<Point> points = new ArrayList<>();
 
         int dx = p2.x - p1.x;
         int dy = p2.y - p1.y;
+
+        // Delta & Slope Calculation
+
+        int steps = Math.max(Math.abs(dx), Math.abs(dy));
+
+        float xInc = dx / (float) steps;
+        float yInc = dy / (float) steps;
+
+        float x = p1.x;
+        float y = p1.y;
 
         return points.toArray(new Point[0]);
     }
