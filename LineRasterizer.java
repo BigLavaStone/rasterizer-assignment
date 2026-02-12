75aafc1895a15f18940e8a30fe9cf42f8454bf2f
bigLavaStone <mrityunjoysahoo007@gmail.com>
2026-02-12 23:34:47 +0530

    Added POint class in this file for compilation

--- LineRasterizer.java
+++ LineRasterizer.java
@@ -1,3 +1,12 @@
+class Point {
+    int x, y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 interface LineRasterizer {
     Point[] rasterize(Point p1, Point p2);
 }
